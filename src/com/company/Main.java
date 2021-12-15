34d988349e75ec9bb2c3910e06885be3fee9c2b2
Awahab671 <abdulwahab.rahmati1@gmail.com>
2021-12-14 22:47:23 -0800

    commiting new code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i = 0; i <10; i++) {
-            System.out.println("Wahab is trying his best");
+        for (int i = 0; i <4; i++) {
+            System.out.println("Jenkins Project");
             Thread.sleep(2000);
         }
     }
