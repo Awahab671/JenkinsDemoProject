99af5ce44643e405b21bdc03f3e60bfd6cf1ba02
Awahab671 <abdulwahab.rahmati1@gmail.com>
2021-12-14 22:42:30 -0800

    Third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         for (int i = 0; i <10; i++) {
-            System.out.println("Wahab is doing great");
+            System.out.println("Wahab is trying his best");
             Thread.sleep(2000);
         }
     }
